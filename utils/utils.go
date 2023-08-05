e2b0b09636f6ad9616877656d3e8cb10128268fa
Karma Riuk <riukkarma@gmail.com>
2023-08-05 16:15:54 +0200

    Added min function to utils to get min between two comparable types (how isn't it in the STL?)

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,21 @@
 package utils
 
-import "log"
+import (
+	"log"
+
+	"golang.org/x/exp/constraints"
+)
 
 func Check(err error, msg string, args ...any) {
 	if err != nil {
 		log.Printf(msg, args...)
 		panic(err)
 	}
+}
+
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
