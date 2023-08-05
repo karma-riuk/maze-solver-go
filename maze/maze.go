19ab3c9cf3dffe3324e7163a4f4f015705e28967
Karma Riuk <riukkarma@gmail.com>
2023-08-05 11:20:53 +0200

    Added String() method to RawMaze for debugging purposes

--- maze/maze.go
+++ maze/maze.go
@@ -1,5 +1,10 @@
 package maze
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Coordinates struct {
 	X, Y int
 }
@@ -22,6 +27,23 @@ func NewNode(coords Coordinates) *Node {
 type RawMaze struct {
 	PathChar, WallChar byte
 	Data               []string
+}
+
+func (m *RawMaze) String() string {
+	var ret strings.Builder
+	ret.WriteString("{\n")
+	ret.WriteString(fmt.Sprintf("\tPathChar: %v,\n", m.PathChar))
+	ret.WriteString(fmt.Sprintf("\tWallChar: %v,\n", m.WallChar))
+	ret.WriteString("\tData: \n")
+	for _, line := range m.Data {
+		ret.WriteRune('\t')
+		ret.WriteRune('\t')
+		ret.WriteString(line)
+		ret.WriteRune('\n')
+	}
+	ret.WriteString("}")
+
+	return ret.String()
 }
 
 type Maze struct {
