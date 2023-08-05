c8e517f73c51985f258a570030f7a35670bc38c6
Karma Riuk <riukkarma@gmail.com>
2023-08-05 11:25:32 +0200

    Moved RaMaze to its own file, cuz we gonna need some more functions and it would just clutter maze.go

---
+++ maze/raw_maze.go
@@ -0,0 +1,28 @@
+package maze
+
+import (
+	"fmt"
+	"strings"
+)
+
+type RawMaze struct {
+	PathChar, WallChar byte
+	Data               []string
+}
+
+func (m *RawMaze) String() string {
+	var ret strings.Builder
+	ret.WriteString("{\n")
+	ret.WriteString(fmt.Sprintf("\tPathChar: %v,\n", m.PathChar))
+	ret.WriteString(fmt.Sprintf("\tWallChar: %v,\n", m.WallChar))
+	ret.WriteString("\tData: \n")
+	for _, line := range m.Data {
+		ret.WriteRune('\t')
+		ret.WriteRune('\t')
+		ret.WriteString(line)
+		ret.WriteRune('\n')
+	}
+	ret.WriteString("}")
+
+	return ret.String()
+}
